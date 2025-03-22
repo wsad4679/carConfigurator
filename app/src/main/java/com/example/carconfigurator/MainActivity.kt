5b2fe64f74e3a695955bcae7f72b5b63f869e67c
SzymonH <szymonh4679@gmail.com>
2025-03-22 19:45:51 +0100

    finished backend

--- app/src/main/java/com/example/carconfigurator/MainActivity.kt
+++ app/src/main/java/com/example/carconfigurator/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.carconfigurator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +22,73 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val carImageView : ImageView = findViewById(R.id.car_imageview)
+
+        val carImages = listOf(
+            R.drawable.sedan,
+            R.drawable.suv,
+            R.drawable.hatchback
+        )
+        carImageView.setImageResource(carImages[0])
+
+        var carTypeRadioGroup : RadioGroup = findViewById(R.id.car_type_radiogorup)
+        var carType = "Sedan"
+
+        carTypeRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+
+            val carTypeRadioButton : RadioButton = findViewById(checkedId)
+            carType = "${carTypeRadioButton.text}"
+
+            carImageView.setImageResource(
+                when(carTypeRadioButton.text){
+
+                    "Sedan"-> carImages[0]
+                    "SUV" -> carImages[1]
+                    "Hatchback" ->carImages[2]
+                    else -> carImages[0]
+                }
+            )
+        }
+
+
+        val coolSeatCheckBox : CheckBox = findViewById(R.id.cool_seats_checkbox)
+        var coolSeat = "podgrzewane"
+        coolSeatCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked)
+            {
+                coolSeat = "chłodzone i podgrzewane"
+            }
+            else
+            {
+                coolSeat = "podgrzewane"
+            }
+        }
+
+        val leatherSeatCheckBox : CheckBox = findViewById(R.id.leather_seats_checkbox)
+        var leatherSeat = "sztuczna skóra"
+        leatherSeatCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked)
+            {
+                leatherSeat = "skórzane"
+            }
+            else
+            {
+                leatherSeat = "sztuczna skóra"
+            }
+        }
+
+        val orderTextView : TextView = findViewById(R.id.order_textview)
+
+        val orderButton : Button = findViewById(R.id.order_button)
+
+        orderButton.setOnClickListener {
+            orderTextView.text = "Poddsumowanie zamówienia: \n Rodzaj samochodu: $carType \n Fotele: $coolSeat, $leatherSeat"
+        }
+
+
+
+
+
     }
 }
